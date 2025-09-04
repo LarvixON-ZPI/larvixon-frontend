59f492924cfc45f1edfdb04dbbbd1aa77dd75c76
Wasloso <dziunkowski@gmail.com>
2025-09-04 13:03:45 +0200

    feat: update routing and authentication flow to include LandingPage and pass initial parameters to AuthForm

--- lib/src/authentication/presentation/auth_page.dart
+++ lib/src/authentication/presentation/auth_page.dart
@@ -1,18 +1,19 @@
-import 'dart:io';
-
 import 'package:flutter/material.dart';
-import 'package:flutter/widget_previews.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:go_router/go_router.dart';
-import 'package:larvixon_frontend/src/authentication/presentation/auth_form.dart';
 
-import '../bloc/auth_bloc.dart';
+import '../../landing/landing_page.dart';
+import 'auth_form.dart';
 
 class AuthPage extends StatefulWidget {
   static const String route = '/login';
   static const String name = 'login';
+  final AuthFormMode initialMode;
+  final String? initialEmail;
 
-  const AuthPage({super.key});
+  const AuthPage({
+    super.key,
+    this.initialMode = AuthFormMode.signUp,
+    this.initialEmail,
+  });
 
   @override
   State<AuthPage> createState() => _AuthPageState();
@@ -26,10 +27,23 @@ class _AuthPageState extends State<AuthPage>
       builder: (context, constraints) {
         final isWide = constraints.maxWidth > 800;
         return Scaffold(
-          body: Row(
+          backgroundColor: Colors.transparent,
+          body: Stack(
+            fit: StackFit.expand,
             children: [
-              if (isWide) Expanded(flex: 3, child: Placeholder()),
-              Flexible(flex: isWide ? 2 : 1, child: AuthForm()),
+              Background(),
+              Row(
+                children: [
+                  if (isWide) Expanded(flex: 3, child: Placeholder()),
+                  Flexible(
+                    flex: isWide ? 2 : 1,
+                    child: AuthForm(
+                      initialMode: widget.initialMode,
+                      initialEmail: widget.initialEmail,
+                    ),
+                  ),
+                ],
+              ),
             ],
           ),
         );
