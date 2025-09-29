d2bf8afd3e4d319bf42f4bdf55596c543e1e2995
Wasloso <dziunkowski@gmail.com>
2025-09-29 11:50:45 +0200

    refactor: enhance CustomCard layout with background support for future use

--- lib/src/common/widgets/custom_card.dart
+++ lib/src/common/widgets/custom_card.dart
@@ -10,6 +10,7 @@ class CustomCard extends StatelessWidget {
     this.mainAxisSize = MainAxisSize.min,
     this.useWrap = true,
     this.color,
+    this.background,
   });
   final String? title;
   final String? description;
@@ -18,40 +19,48 @@ class CustomCard extends StatelessWidget {
   final MainAxisSize mainAxisSize;
   final bool useWrap;
   final Color? color;
+  final Widget? background;
 
   @override
   Widget build(BuildContext context) {
     return Card(
+      clipBehavior: Clip.antiAlias,
       color: color ?? Colors.white.withValues(alpha: 0.03),
       shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
-      child: Padding(
-        padding: const EdgeInsets.all(16),
-        child: Column(
-          mainAxisSize: mainAxisSize,
-          spacing: 6,
-          children: [
-            if (icon != null)
-              Column(children: [Icon(icon, size: 40), SizedBox(height: 2)]),
-            if (title != null)
-              Text(title!, style: Theme.of(context).textTheme.titleMedium),
-            if (description != null)
-              Column(
-                children: [
-                  Text(
-                    description!,
-                    textAlign: TextAlign.center,
-                    style: Theme.of(context).textTheme.bodyMedium,
-                  ),
-                ],
-              ),
+      child: Stack(
+        children: [
+          if (background != null) Positioned.fill(child: background!),
 
-            if (children != null)
-              if (useWrap) ...[
-                Wrap(spacing: 12, runSpacing: 12, children: children!),
-              ] else
-                ...children!,
-          ],
-        ),
+          Padding(
+            padding: const EdgeInsets.all(16),
+            child: Column(
+              mainAxisSize: mainAxisSize,
+              spacing: 6,
+              children: [
+                if (icon != null)
+                  Column(children: [Icon(icon, size: 40), SizedBox(height: 2)]),
+                if (title != null)
+                  Text(title!, style: Theme.of(context).textTheme.titleMedium),
+                if (description != null)
+                  Column(
+                    children: [
+                      Text(
+                        description!,
+                        textAlign: TextAlign.center,
+                        style: Theme.of(context).textTheme.bodyMedium,
+                      ),
+                    ],
+                  ),
+
+                if (children != null)
+                  if (useWrap) ...[
+                    Wrap(spacing: 12, runSpacing: 12, children: children!),
+                  ] else
+                    ...children!,
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
