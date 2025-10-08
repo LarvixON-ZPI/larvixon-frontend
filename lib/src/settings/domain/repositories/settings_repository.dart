639287f3fb8dc88736f7244ce95e1fc4f2acacd6
Patryk Łuszczek <52531090+Wasloso@users.noreply.github.com>
2025-10-08 16:28:39 +0200

    feat: implemented settings cubit along with settings repository, allows for changing global state of language and theme

---
+++ lib/src/settings/domain/repositories/settings_repository.dart
@@ -0,0 +1,12 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+import 'package:fpdart/fpdart.dart';
+import 'package:larvixon_frontend/core/errors/failures.dart';
+
+abstract class SettingsRepository {
+  TaskEither<Failure, Locale> getLanguage();
+  TaskEither<Failure, ThemeMode> getThemeMode();
+  TaskEither<Failure, void> setThemeMode({required ThemeMode themeMode});
+  TaskEither<Failure, void> setLanguage(Locale locale);
+}
---
+++ lib/src/settings/presentation/blocs/cubit/settings_cubit.dart
@@ -0,0 +1,75 @@
+// ignore: depend_on_referenced_packages
+import 'package:bloc/bloc.dart';
+import 'package:equatable/equatable.dart';
+import 'package:flutter/material.dart';
+import 'package:fpdart/fpdart.dart';
+import 'package:larvixon_frontend/core/errors/failures.dart';
+import 'package:larvixon_frontend/src/settings/domain/repositories/settings_repository.dart';
+
+part 'settings_state.dart';
+
+class SettingsCubit extends Cubit<SettingsState> {
+  final SettingsRepository repository;
+  final List<Locale> supportedLocales;
+  SettingsCubit({required this.repository, required this.supportedLocales})
+    : super(SettingsState(locale: Locale('en'), theme: ThemeMode.system));
+
+  Future<void> loadSettings() async {
+    // Maybe they should be chained?
+    final localeResult = await repository.getLanguage().run();
+    final themeResult = await repository.getThemeMode().run();
+
+    localeResult.match((failure) {}, (locale) {
+      final isSupported = supportedLocales.contains(locale);
+      emit(state.copyWith(locale: isSupported ? locale : Locale('en')));
+    });
+
+    themeResult.match((failure) {}, (theme) {
+      emit(state.copyWith(theme: theme));
+    });
+    print("theme: ${themeResult}, locale: ${localeResult}");
+  }
+
+  Future<void> setLocale({required Locale locale}) async {
+    if (!supportedLocales.contains(locale)) return;
+    final localeResult = await repository.setLanguage(locale).run();
+    localeResult.match((failure) {}, (success) {
+      emit(state.copyWith(locale: locale));
+    });
+  }
+
+  Future<void> toggleTheme() async {
+    ThemeMode toggleTo;
+    switch (state.theme) {
+      case ThemeMode.system:
+        toggleTo =
+            WidgetsBinding.instance.platformDispatcher.platformBrightness ==
+                Brightness.dark
+            ? ThemeMode.light
+            : ThemeMode.dark;
+      case ThemeMode.light:
+        toggleTo = ThemeMode.dark;
+      case ThemeMode.dark:
+        toggleTo = ThemeMode.light;
+    }
+
+    final result = await repository
+        .setThemeMode(themeMode: toggleTo)
+        .flatMap((_) => repository.getThemeMode())
+        .run();
+
+    result.match((e) {}, (theme) {
+      emit(state.copyWith(theme: theme));
+    });
+  }
+
+  Future<void> setTheme({required ThemeMode theme}) async {
+    final result = await repository
+        .setThemeMode(themeMode: theme)
+        .flatMap((_) => repository.getThemeMode())
+        .run();
+    result.match((e) {}, (theme) {
+      emit(state.copyWith(theme: theme));
+    });
+  }
+}
