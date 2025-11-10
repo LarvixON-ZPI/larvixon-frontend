6b1f4a78beca9e0dcd2bc62eab5dc6e14765d366
Mikołaj Kubś <mikikubs@gmail.com>
2025-11-10 23:27:29 +0100

    feat: final simulation version

--- web/UnityLibrary/Build/UnityLibrary.loader.js
+++ web/UnityLibrary/Build/UnityLibrary.loader.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d653127ef290ccee16f76b563b8754f102c83be1f0333a4936546cd61a9b79d9
+oid sha256:ae1ef068e519e25e011749b5427bbeefc0d26704860fd13293221e9123eacb9a
 size 26454
