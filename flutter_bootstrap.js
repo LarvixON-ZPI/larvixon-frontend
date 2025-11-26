cb85ec24bc375a00b1a47515c406c4da7c36d142
Wasloso <Wasloso@users.noreply.github.com>
2025-11-26 12:14:42 +0000

    deploy: 37cd3df85a2611c9d521727a7fd5e3b4dd18c472

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"d3d45dcf251823c1769909cd43698d126db38d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1534380601"
+    serviceWorkerVersion: "1116731531"
   }
 });
